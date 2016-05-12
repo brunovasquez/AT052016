fd7c82cc8817ba38f28b9e8f0991d18ebecbd221
brunovasquez <Bruno.Vasquez@fundacion-jala.org>
2016-05-12 17:37:11 -0400

    Correxion

--- Bruno/Task 06/def_method_block.rb
+++ Bruno/Task 06/def_method_block.rb
@@ -1,15 +1,15 @@
 def method_block
-result=yield
+result =0
+result=yield result
 
 puts "The value obtained is #{result}"
 end 
 
 method_block {result=10}
 
-method_block do 
-	otro=0
-result = otro!=0? 25+15 : 25*5
-#puts "I changed the value into the block"
+method_block do |result|
+puts "I changed the value into the block"	
+result = result==0? 25+15 : 25*5
 end
 
 method_block {result = 15 /25}
